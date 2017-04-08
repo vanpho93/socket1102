b305cea4fa44736468da6155b0afbf79d675a14a
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-08 13:53:11 +0700

    init

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.listen(3000, () => console.log('server started'));
+server.listen(3000, () => console.log('server started'));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.render('home'));
